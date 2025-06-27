9f48b1b9e12c90ce4baa0f51daa03b7091e55e97
craftsmandigital <htcjon10@gmail.com>
2025-06-27 13:05:31 +0200

    Added general grouping on ticker with the dataframe "explode" trick"

--- general_trader.py
+++ general_trader.py
@@ -1,13 +1,17 @@
 import polars as pl
+# from polars import selectors as cs
 from typing import Dict
-from datetime import datetime
+# from datetime import datetime
 
 class GeneralTrader:
     """
     A class to represent a general trading strategy framework.
     """
 
-    def __init__(self, df_ohlc: pl.DataFrame):
+    def __init__(self,
+                 df_ohlc: pl.DataFrame,
+                 filter_expr: pl.Expr | None = None
+                 ):
         """
         Initializes the GeneralTrader.
 
@@ -16,12 +20,13 @@ class GeneralTrader:
                                     and potentially other market data.
         """
         # self.df_ohlc = df_ohlc
-        self.pre_filter : pl.Expr = None
         self.added_columns : Dict[str, pl.Expr] = {}
         self.df_ohlc : pl.DataFrame = df_ohlc
+        self.df_ohlc_initial : pl.DataFrame = df_ohlc
+        self.pre_filter = self.add_pre_filter(filter_expr)
 
 
-    def add_pre_filter(self, filter_expr: pl.Expr) -> "GeneralTrader":
+    def add_pre_filter(self, filter_expr: pl.Expr | None) -> "GeneralTrader":
         """
         Apply a filter expression to the internal OHLC DataFrame.
 
@@ -29,13 +34,24 @@ class GeneralTrader:
         ----------
         filter_expr : pl.Expr
             A Polars expression used to filter the DataFrame rows.
+            Here is an example of a typical filter date and ticker:
+            (
+                pl.col("date").is_between(
+                datetime(2019, 5, 22), datetime(2024, 5, 22), closed="none"
+                ) & (
+                pl.col("ticker").is_in(["MSFT", "AAPL", "TSLA"])
+                )
+            )
 
         Returns
         -------
         GeneralTrader
             Returns self to allow method chaining.
         """
-        self.df_ohlc = self.df_ohlc.filter(filter_expr)
+        if filter_expr is None:
+            return self
+        self.df_ohlc_initial = self.df_ohlc_initial.filter(filter_expr)
+        self.df_ohlc = self.df_ohlc_initial
         return self
 
     def add_column(self, name: str, expr: pl.Expr) -> "GeneralTrader":
@@ -69,9 +85,27 @@ class GeneralTrader:
         GeneralTrader
             Returns self to allow method chaining.
         """
+        # initial_columns :list[str] = self.df_ohlc.columns
+
         if self.added_columns:
             try:
-                self.df_ohlc = self.df_ohlc.with_columns(**self.added_columns)
+                # 1. Group & compute
+                df_group = (
+                    self.df_ohlc
+                    .group_by(
+                        pl.col("ticker"), maintain_order=True
+                    )
+                    .agg(pl.all(), **self.added_columns)
+                )
+
+                # 2. Getting all collumns of List data type.
+                list_cols = [c for c, t in df_group.schema.items() if isinstance(t, pl.List)]
+
+                # 3. This will explode all columns that are of the List data type.
+                self.df_ohlc = (
+                    df_group
+                    .explode(list_cols)
+                )
             except Exception as e:
                 print(f"Error during apply_added_columns(): {e}")
                 raise
